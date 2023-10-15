716b2b5b2347878950909049bbd12fa96347e442
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-15 12:20:07 -0500

    +

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.scss';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,7 +15,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem storageKey='golden-age-minecraft-theme'>
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,27 @@
-import { UserButton } from '@clerk/nextjs';
+'use client';
+
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { redirect, useRouter } from 'next/navigation';
 import { FC } from 'react';
+import { ModeToggle } from '@/components/mode-toggle';
 
 const Navbar: FC = () => {
+  const router = useRouter();
+
   return (
-    <div className='bg-neutral-900 py-2 px-4 w-full'>
+    <div className='bg-neutral-900 py-2 px-4 w-full flex gap-2'>
+      <div className='max-h-[32px] grid place-items-center'>
+        <ModeToggle />
+      </div>
       <div className='ml-auto w-max'>
-        <UserButton afterSignOutUrl='/' />
+        <SignedIn>
+          <UserButton afterSignOutUrl='/' />
+        </SignedIn>
+        <SignedOut>
+          <button onClick={() => router.push('/sign-in')} className='bg-emerald-500 rounded-md px-2 hover:bg-emerald-800 transition h-[32px]'>
+            <p>Sign in</p>
+          </button>
+        </SignedOut>
       </div>
     </div>
   );
