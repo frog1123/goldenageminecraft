5ded8aa09ce957aaa0fd5f0d824f1a4afd915cf0
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-17 19:50:52 -0500

    +

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 
 const Home: NextPage = async () => {
   return (
     <div className='grid grid-flow-row gap-2 w-[60%] mx-auto'>
-      <p>home</p>
+      <div className='bg-neutral-200 dark:bg-neutral-900 p-2 rounded-md'>
+        <Link href='/forums'>
+          <div className='hover:bg-neutral-300 dark:hover:bg-neutral-800 p-1 transition rounded-md w-max'>forums</div>
+        </Link>
+      </div>
     </div>
   );
 };
