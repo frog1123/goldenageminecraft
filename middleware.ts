767c879b9a8b2584f10bda27f2d3c3694efc33f6
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-21 09:44:16 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,8 @@ import { authMiddleware } from '@clerk/nextjs';
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 
 export default authMiddleware({
-  publicRoutes: ['/api/threads', '/', '/forums', '/forums/threads', '/forums/threads/:threadId', '/users/:userId']
+  publicRoutes: ['/api/threads', '/', '/forums', '/forums/threads', '/forums/threads/:threadId', '/users/:userId'],
+  ignoredRoutes: ['/api/users'] // temp
 });
 
 export const config = {
