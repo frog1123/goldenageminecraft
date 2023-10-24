85085e7ed2e8623fb1a57db949ba1b318749bbdf
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-24 16:57:00 -0500

    +

--- app/(main)/users/[userId]/page.tsx
+++ app/(main)/users/[userId]/page.tsx
@@ -1,4 +1,5 @@
 import Threads from '@/components/threads/threads';
+import UserInfo from '@/components/users/user-info';
 import { NextPage } from 'next';
 
 interface UserIdPageProps {
@@ -10,6 +11,7 @@ interface UserIdPageProps {
 const UserIdPage: NextPage<UserIdPageProps> = ({ params }) => {
   return (
     <div className='grid grid-flow-row gap-2 w-full sm:w-[60%] lg:w-[50%] xl:w-[40%] mx-auto'>
+      <UserInfo />
       <Threads authorId={params.userId} />
     </div>
   );
---
+++ components/users/user-info.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const UserInfo: FC = () => {
+  return (
+    <div>
+      <p>user info</p>
+    </div>
+  );
+};
+
+export default UserInfo;
