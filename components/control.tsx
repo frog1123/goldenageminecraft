b5bf67c03a36fa622d5ae63cde72800460c9df3a
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-26 17:12:02 -0500

    Update control.tsx

--- components/control.tsx
+++ components/control.tsx
@@ -10,11 +10,14 @@ const Control: FC = () => {
   return (
     <div className='bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2 overflow-auto grid grid-cols-[auto_max-content] gap-2'>
       <div>
-        <input placeholder='Search' className='w-full h-full rounded-md px-2' />
+        <input
+          placeholder='Search'
+          className='w-full h-full bg-zinc-300/50 dark:bg-neutral-800 border-0 focus-visible:ring-0 text-black dark:text-white outline-none p-2 rounded-md'
+        />
       </div>
       <div className='ml-auto'>
         <Link href='/forums/threads/create'>
-          <button className='bg-emerald-500 rounded-md px-2 hover:bg-emerald-800 transition h-[32px]'>
+          <button className='bg-emerald-500 rounded-md px-2 hover:bg-emerald-800 transition h-full'>
             <p>Create thread</p>
           </button>
         </Link>
