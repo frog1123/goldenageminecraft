d7228fa1e749b5782612ffbe6baf1d0f41556a3b
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-26 19:50:19 -0500

    Update page.tsx

--- app/(main)/forums/tags/[tagId]/page.tsx
+++ app/(main)/forums/tags/[tagId]/page.tsx
@@ -11,9 +11,12 @@ interface TagIdPageProps {
 const TagIdPage: NextPage<TagIdPageProps> = async ({ params }) => {
   return (
     <div className='grid grid-flow-row gap-2 w-[40%] mx-auto'>
-      <div className='bg-blue-500/25 p-2 rounded-md grid grid-cols-[max-content_max-content] place-items-center w-max mx-auto'>
-        <Hash className='w-6 h-6' />
-        <span className='text-2xl'>{params.tagId}</span>
+      <div className='grid grid-flow-col w-max gap-2 place-items-center'>
+        <span className='text-lg font-semibold'>Posts containing</span>
+        <div className='bg-blue-500/25 p-1 rounded-md grid grid-cols-[max-content_max-content] place-items-center w-max mx-auto'>
+          <Hash className='w-5 h-5' />
+          <span className='text-lg'>{params.tagId}</span>
+        </div>
       </div>
       <TagThreads tagId={params.tagId} />
     </div>
