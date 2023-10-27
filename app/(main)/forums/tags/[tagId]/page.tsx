c789161827e71b4a430814d36ce3a136e126d6e8
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-26 19:32:51 -0500

    Update page.tsx

--- app/(main)/forums/tags/[tagId]/page.tsx
+++ app/(main)/forums/tags/[tagId]/page.tsx
@@ -1,3 +1,4 @@
+import { Hash } from 'lucide-react';
 import { NextPage } from 'next';
 
 interface TagIdPageProps {
@@ -8,8 +9,11 @@ interface TagIdPageProps {
 
 const TagIdPage: NextPage<TagIdPageProps> = async ({ params }) => {
   return (
-    <div>
-      <p>tag id: {params.tagId}</p>
+    <div className='grid grid-flow-row gap-2 w-[40%] mx-auto'>
+      <div className='bg-blue-500/25 p-2 rounded-md grid grid-cols-[max-content_max-content] place-items-center w-max mx-auto'>
+        <Hash className='w-6 h-6' />
+        <span className='text-2xl'>{params.tagId}</span>
+      </div>
     </div>
   );
 };
