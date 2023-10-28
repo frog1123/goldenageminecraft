44c4f4f8647375a779f674853b61913a0f13ae31
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-28 11:16:05 -0500

    Update navbar.tsx

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -32,6 +32,24 @@ const Navbar: FC = () => {
 
   const { setTheme } = useTheme();
 
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   useEffect(() => {
     if (!user?.id) return;
 
@@ -44,113 +62,116 @@ const Navbar: FC = () => {
   });
 
   return (
-    <div className='bg-neutral-200 dark:bg-neutral-900 py-2 px-4 w-full flex gap-2 fixed z-50'>
-      <div className='grid grid-flow-col place-items-center'>
-        <div className='grid grid-flow-col gap-2 place-items-center'>
-          <Link href='/'>
-            <div className='hover:bg-neutral-300 dark:hover:bg-neutral-800 p-1 transition rounded-md'>
-              <div className='w-5 h-5'>
-                <Image src={grass_block_old} alt='logo' />
-              </div>
-            </div>
-          </Link>
-          {pathname !== '/' && <ChevronRight className='navbar-routes w-4 h-4 text-gray-500' />}
-        </div>
-        {pathnames.map((path, index) => {
-          let routeTo = `${pathnames.slice(0, index + 1).join('/')}`;
-
-          if (routeTo === '' || routeTo === '/') return null;
-
-          return (
-            <div className={cn('navbar-routes grid grid-flow-col place-items-center gap-2', index !== 0 && 'ml-2')} key={path}>
-              <Path path={path} route={routeTo} />
-              {index !== 0 && index !== pathnames.length - 1 && <ChevronRight className='w-4 h-4 text-gray-500' />}
-            </div>
-          );
-        })}
-      </div>
-      <div className='ml-auto w-max grid place-items-center grid-flow-col'>
-        <SignedIn>
-          {user && (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <div className='h-8 w-8 aspect-square rounded-[50%] overflow-hidden box-border cursor-pointer'>
-                  <img src={user.imageUrl} alt='author' />
+    <div className='fixed z-50 w-full'>
+      <div className='bg-neutral-200 dark:bg-neutral-900 py-2 px-4 w-full flex gap-2'>
+        <div className='grid grid-flow-col place-items-center'>
+          <div className='grid grid-flow-col gap-2 place-items-center'>
+            <Link href='/'>
+              <div className='hover:bg-neutral-300 dark:hover:bg-neutral-800 p-1 transition rounded-md'>
+                <div className='w-5 h-5'>
+                  <Image src={grass_block_old} alt='logo' />
                 </div>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align='end'>
-                <DropdownMenuItem className='pointer-events-none'>
-                  <div>
-                    <span>{user.fullName ? user.fullName : user.username}</span>
-                  </div>
-                </DropdownMenuItem>
-                <Separator className='my-1' />
-                <DropdownMenuItem onClick={() => signOut()}>
-                  <div className='flex place-items-center w-full gap-2'>
-                    <span>Sign out</span>
-                    <LogOut className='w-4 h-4 ml-auto' />
-                  </div>
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => openUserProfile()}>
-                  <div className='flex place-items-center w-full gap-2'>
-                    <span>Manage account</span>
-                    <Wrench className='w-4 h-4 ml-auto' />
-                  </div>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Link href={`/users/${id}`} className='w-full'>
-                    <div className='flex place-items-center w-full gap-2'>
-                      <span>View profile</span>
-                      <User className='w-4 h-4 ml-auto' />
-                    </div>
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuSub>
-                  <DropdownMenuSubTrigger className='flex items-center'>
-                    <span>Set theme</span>
-                  </DropdownMenuSubTrigger>
-                  <DropdownMenuPortal>
-                    <DropdownMenuSubContent>
-                      <DropdownMenuItem onClick={() => setTheme('light')}>
-                        <div className='flex place-items-center w-full gap-2'>
-                          <span>Light</span>
-                          <Sun className='w-4 h-4 ml-auto' />
-                        </div>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem onClick={() => setTheme('dark')}>
-                        <div className='flex place-items-center w-full gap-2'>
-                          <span>Dark</span>
-                          <Moon className='w-4 h-4 ml-auto' />
-                        </div>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem onClick={() => setTheme('system')}>
-                        <div className='flex place-items-center w-full gap-2'>
-                          <span>System</span>
-                          <Settings className='w-4 h-4 ml-auto' />
-                        </div>
-                      </DropdownMenuItem>
-                    </DropdownMenuSubContent>
-                  </DropdownMenuPortal>
-                </DropdownMenuSub>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )}
-        </SignedIn>
-        <SignedOut>
-          <div className='grid grid-cols-[max-content_max-content] gap-2'>
-            <Link href='/sign-up'>
-              <button className='bg-emerald-500 rounded-md px-2 hover:bg-emerald-800 transition h-[32px]'>
-                <p className='text-white'>Sign up</p>
-              </button>
+              </div>
             </Link>
-            <Link href='/sign-in'>
-              <button className='bg-emerald-500 rounded-md px-2 hover:bg-emerald-800 transition h-[32px]'>
-                <p className='text-white'>Sign in</p>
-              </button>
-            </Link>
+            {pathname !== '/' && <ChevronRight className='navbar-routes w-4 h-4 text-gray-500' />}
           </div>
-        </SignedOut>
+          {pathnames.map((path, index) => {
+            let routeTo = `${pathnames.slice(0, index + 1).join('/')}`;
+
+            if (routeTo === '' || routeTo === '/') return null;
+
+            return (
+              <div className={cn('navbar-routes grid grid-flow-col place-items-center gap-2', index !== 0 && 'ml-2')} key={path}>
+                <Path path={path} route={routeTo} />
+                {index !== 0 && index !== pathnames.length - 1 && <ChevronRight className='w-4 h-4 text-gray-500' />}
+              </div>
+            );
+          })}
+        </div>
+        <div className='ml-auto w-max grid place-items-center grid-flow-col'>
+          <SignedIn>
+            {user && (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <div className='h-8 w-8 aspect-square rounded-[50%] overflow-hidden box-border cursor-pointer'>
+                    <img src={user.imageUrl} alt='author' />
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align='end'>
+                  <DropdownMenuItem className='pointer-events-none'>
+                    <div>
+                      <span>{user.fullName ? user.fullName : user.username}</span>
+                    </div>
+                  </DropdownMenuItem>
+                  <Separator className='my-1' />
+                  <DropdownMenuItem onClick={() => signOut()}>
+                    <div className='flex place-items-center w-full gap-2'>
+                      <span>Sign out</span>
+                      <LogOut className='w-4 h-4 ml-auto' />
+                    </div>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => openUserProfile()}>
+                    <div className='flex place-items-center w-full gap-2'>
+                      <span>Manage account</span>
+                      <Wrench className='w-4 h-4 ml-auto' />
+                    </div>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link href={`/users/${id}`} className='w-full'>
+                      <div className='flex place-items-center w-full gap-2'>
+                        <span>View profile</span>
+                        <User className='w-4 h-4 ml-auto' />
+                      </div>
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuSub>
+                    <DropdownMenuSubTrigger className='flex items-center'>
+                      <span>Set theme</span>
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent>
+                        <DropdownMenuItem onClick={() => setTheme('light')}>
+                          <div className='flex place-items-center w-full gap-2'>
+                            <span>Light</span>
+                            <Sun className='w-4 h-4 ml-auto' />
+                          </div>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme('dark')}>
+                          <div className='flex place-items-center w-full gap-2'>
+                            <span>Dark</span>
+                            <Moon className='w-4 h-4 ml-auto' />
+                          </div>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme('system')}>
+                          <div className='flex place-items-center w-full gap-2'>
+                            <span>System</span>
+                            <Settings className='w-4 h-4 ml-auto' />
+                          </div>
+                        </DropdownMenuItem>
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+          </SignedIn>
+          <SignedOut>
+            <div className='grid grid-cols-[max-content_max-content] gap-2'>
+              <Link href='/sign-up'>
+                <button className='bg-emerald-500 rounded-md px-2 hover:bg-emerald-800 transition h-[32px]'>
+                  <p className='text-white'>Sign up</p>
+                </button>
+              </Link>
+              <Link href='/sign-in'>
+                <button className='bg-emerald-500 rounded-md px-2 hover:bg-emerald-800 transition h-[32px]'>
+                  <p className='text-white'>Sign in</p>
+                </button>
+              </Link>
+            </div>
+          </SignedOut>
+        </div>
       </div>
+      <Separator className={cn('transition-all duration-500 mx-auto', scrolled ? 'w-full visible' : 'w-0 invisible')} />
     </div>
   );
 };
