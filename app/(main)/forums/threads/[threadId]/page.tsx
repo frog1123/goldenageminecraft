ad9bb84d1671d2a29d8f157646ec049d06d9a18e
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-28 10:55:44 -0500

    Update page.tsx

--- app/(main)/forums/threads/[threadId]/page.tsx
+++ app/(main)/forums/threads/[threadId]/page.tsx
@@ -4,6 +4,16 @@ import { db } from '@/lib/db';
 import { formatDateLong } from '@/utils/format-date-long';
 import { NextPage } from 'next';
 import Image from 'next/image';
+import { UserRank, UserRole, UserPlan } from '@prisma/client';
+
+import coal from '@/public/assets/ranks/coal.png';
+import iron from '@/public/assets/ranks/iron.png';
+import gold from '@/public/assets/ranks/gold.png';
+import redstone from '@/public/assets/ranks/redstone.png';
+import lapis from '@/public/assets/ranks/lapis.png';
+import diamond from '@/public/assets/ranks/diamond.png';
+import { Crown, Gavel, Sailboat, Shield } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 interface ThreadIdPageProps {
   params: {
@@ -22,11 +32,46 @@ const ThreadIdPage: NextPage<ThreadIdPageProps> = async ({ params }) => {
           name: true,
           imageUrl: true,
           createdAt: true,
+          rank: true,
+          role: true,
+          plan: true,
           _count: true
         }
       }
     }
   });
+
+  const rankMap = {
+    [UserRank.COAL]: <Image src={coal} alt='rank' fill />,
+    [UserRank.IRON]: <Image src={iron} alt='rank' fill />,
+    [UserRank.GOLD]: <Image src={gold} alt='rank' fill />,
+    [UserRank.REDSTONE]: <Image src={redstone} alt='rank' fill />,
+    [UserRank.LAPIS]: <Image src={lapis} alt='rank' fill />,
+    [UserRank.DIAMOND]: <Image src={diamond} alt='rank' fill />
+  };
+
+  const rankColorMap = {
+    [UserRank.COAL]: 'bg-neutral-950',
+    [UserRank.IRON]: 'bg-neutral-400',
+    [UserRank.GOLD]: 'bg-yellow-500',
+    [UserRank.REDSTONE]: 'bg-red-800',
+    [UserRank.LAPIS]: 'bg-blue-600',
+    [UserRank.DIAMOND]: 'bg-cyan-500'
+  };
+
+  const roleIconMap = {
+    [UserRole.USER]: null,
+    [UserRole.MODERATOR]: <Shield className='w-5 h-5 text-white' />,
+    [UserRole.ADMIN]: <Gavel className='w-5 h-5 text-white' />,
+    [UserRole.OWNER]: <Sailboat className='w-5 h-5 text-white' />
+  };
+
+  const roleColorMap = {
+    [UserRole.USER]: null,
+    [UserRole.MODERATOR]: 'bg-blue-500',
+    [UserRole.ADMIN]: 'bg-rose-500',
+    [UserRole.OWNER]: 'bg-indigo-700'
+  };
 
   if (!thread) return <PageNotFound message='Thread not found :/' />;
 
@@ -39,12 +84,33 @@ const ThreadIdPage: NextPage<ThreadIdPageProps> = async ({ params }) => {
               <img src={thread.author.imageUrl} alt='author' />
             </div>
             <p>{thread.author.name}</p>
-            <p>Joined {formatDateLong(thread.author.createdAt.toString())}</p>
-            <p>{thread.author._count.threads} Threads</p>
+            <p className='uppercase text-xs font-bold text-zinc-500'>Joined {formatDateLong(thread.author.createdAt.toString())}</p>
+            <p className='uppercase text-xs font-bold text-zinc-500'>{thread.author._count.threads} Threads</p>
+            <div className='w-full rounded-md overflow-hidden'>
+              <div className={cn('grid grid-flow-col grid-cols-[max-content_auto] gap-1 w-full place-items-center p-1', rankColorMap[thread.author.rank])}>
+                <div className='w-6 h-6 overflow-hidden relative'>{rankMap[thread.author.rank]}</div>
+                <span className='mr-auto font-semibold'>{thread.author.rank}</span>
+              </div>
+              {thread.author.role !== 'USER' && (
+                <div className={cn('grid grid-flow-col grid-cols-[max-content_auto] gap-1 w-full place-items-center p-1', roleColorMap[thread.author.role])}>
+                  <div className='w-6 h-6 overflow-hidden relative'>{roleIconMap[thread.author.role]}</div>
+                  <span className='mr-auto font-semibold'>{thread.author.role}</span>
+                </div>
+              )}
+              {thread.author.plan === 'PREMIUM' && (
+                <div className='grid grid-flow-col grid-cols-[max-content_auto] gap-1 w-full place-items-center bg-pink-500 p-1'>
+                  <div className='w-6 h-6 overflow-hidden relative'>
+                    <Crown />
+                  </div>
+                  <span className='mr-auto font-semibold'>{thread.author.plan}</span>
+                </div>
+              )}
+            </div>
           </div>
           <Separator orientation='vertical' />
           <div className=''>
-            <p className='font-semibold text-lg'>{thread?.title}</p>
+            <p className='font-semibold text-lg'>{thread.title}</p>
+            <p>{thread.content}</p>
           </div>
         </div>
       </div>
