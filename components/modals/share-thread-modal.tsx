e29ec09a85fd6aedb45133a6a2720cd78725ad43
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-29 18:37:19 -0500

    +

---
+++ components/modals/share-thread-modal.tsx
@@ -0,0 +1,8 @@
+import { useModal } from '@/hooks/use-modal-store';
+import { FC } from 'react';
+
+export const ShareThreadModal: FC = () => {
+  const modal = useModal();
+
+  return <div>share thread modal</div>;
+};
---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <div>modal</div>
+    </>
+  );
+};
