36d1f4ecd8f224df5b4bd9dbca1f13adebb76e2a
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-30 19:11:24 -0500

    Update page.tsx

--- app/(main)/rules/page.tsx
+++ app/(main)/rules/page.tsx
@@ -1,11 +1,9 @@
-import { Subnav } from '@/components/navigation/subnav';
 import { ClipboardList } from 'lucide-react';
 import { NextPage } from 'next';
 
 const RulesPage: NextPage = () => {
   return (
-    <div className='grid grid-flow-row gap-2 w-full sm:w-[60%] lg:w-[50%] xl:w-[40%] mx-auto'>
-      <Subnav />
+    <>
       <div className='bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2'>
         <div className='grid grid-cols-[max-content_auto] gap-1 place-items-center w-max'>
           <ClipboardList className='w-4 h-4 text-zinc-500' />
@@ -13,7 +11,7 @@ const RulesPage: NextPage = () => {
         </div>
         todo: add rules
       </div>
-    </div>
+    </>
   );
 };
 
