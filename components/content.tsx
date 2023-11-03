7db6c3a0852cf2865c42918e2183ec8bd4cfaca6
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-03 17:00:04 -0500

    +

--- components/content.tsx
+++ components/content.tsx
@@ -7,40 +7,18 @@ interface ContentProps {
 export const Content: FC<ContentProps> = ({ text }) => {
   const makeLinks = (text: string) => {
     const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const imageRegex = /!\[img\]\((.*?)\)/g;
 
-    const imageMatches = text.match(imageRegex);
-    const textWithImageUrls = imageMatches
-      ? text.split(imageRegex).map((part, index) => {
-          if (index % 2 === 0) {
-            return part.split(urlRegex).map((word, index) => {
-              if (word.match(urlRegex)) {
-                return (
-                  <a className='text-blue-500 underline' key={index} href={word}>
-                    {word}
-                  </a>
-                );
-              } else {
-                return <span key={index}>{word}</span>;
-              }
-            });
-          } else {
-            return <img key={index} src={part} alt='img' />;
-          }
-        })
-      : text.split(urlRegex).map((word, index) => {
-          if (word.match(urlRegex)) {
-            return (
-              <a className='text-blue-500 underline' key={index} href={word}>
-                {word}
-              </a>
-            );
-          } else {
-            return <span key={index}>{word}</span>;
-          }
-        });
-
-    return textWithImageUrls;
+    return text.split(urlRegex).map((word, index) => {
+      if (word.match(urlRegex)) {
+        return (
+          <a className='text-blue-500 underline' key={index} href={word}>
+            {word}
+          </a>
+        );
+      } else {
+        return <span key={index}>{word}</span>;
+      }
+    });
   };
 
   const textWithLinks = text ? makeLinks(text) : null;
