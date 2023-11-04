8e278e875b2e39e4653b1b16a552f13d9fd93bee
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-04 11:03:45 -0500

    Update page.tsx

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = async () => {
       <Announcement title="Welcome to goldenageminecraft!" content="This is a forum website for discussing all things related to old minecraft!" imageUrls={[img1]} />
       <Announcement title="NOTICE" content="THIS SITE IS IN NO WAY OFFICIALLY AFFILIATED WITH THE /r/GoldenAgeMinecraft SUBREDDIT, MOJANG AB, OR THE MICROSOFT CORPORATION." />
       <Announcement title="NOTICE" content="This website is still in development and the database may be cleared without notice.">
-        <a href="https://github.com/frog1123/goldenageminecraft">
+        <a href="https://github.com/frog1123/goldenageminecraft" target="_blank" rel="noreferrer">
           <button className="bg-indigo-500 hover:bg-indigo-700 p-1 rounded-md transition mt-1">
             <div className="grid grid-cols-[max-content_auto] gap-1 place-items-center">
               <Github className="text-white h-4 w-4" />
