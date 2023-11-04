a27f219442c0de31c9658d81d60169f91640bdc3
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-03 20:09:55 -0500

    +

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,17 +1,7 @@
 "use client";
 
 import { SignIn } from "@clerk/nextjs";
-import { Metadata } from "next";
 import { useEffect, useState } from "react";
-
-export const metadata: Metadata = {
-  title: "Sign in",
-  description: "Sign in to goldenageminecraft",
-  openGraph: {
-    title: "Sign in",
-    description: "Sign in to goldenageminecraft",
-  }
-};
 
 export default function Page() {
   const [isMounted, setIsMounted] = useState(false);
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,17 +1,7 @@
 "use client";
 
 import { SignUp } from "@clerk/nextjs";
-import { Metadata } from "next";
 import { useEffect, useState } from "react";
-
-export const metadata: Metadata = {
-  title: "Sign up",
-  description: "Sign up to goldenageminecraft",
-  openGraph: {
-    title: "Sign up",
-    description: "Sign up to goldenageminecraft"
-  }
-};
 
 export default function Page() {
   const [isMounted, setIsMounted] = useState(false);
