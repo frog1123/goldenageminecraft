a1011cb51b127293fd02b5be5f4a930079a2789f
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-10 16:37:05 -0600

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,13 @@
-import { getCurrentUser } from "@/lib/current-user";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function PATCH(req: Request) {
   try {
-    const currentUser = await getCurrentUser();
-    if (!currentUser) return new NextResponse("Unauthorized", { status: 401 });
+    // if (!currentUser) return new NextResponse("Unauthorized", { status: 401 });
 
     const { id, bio } = await req.json();
 
-    if (id !== currentUser.id) return new NextResponse("Unauthorized", { status: 401 });
+    // if (id !== currentUser.id) return new NextResponse("Unauthorized", { status: 401 });
 
     if (bio.length >= 500) return new NextResponse("Bio too long", { status: 400 });
 
