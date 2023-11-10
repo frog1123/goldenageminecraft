effe1c568fd293937486cf9a8f0143c26a5a20c2
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-10 16:32:29 -0600

    +

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,27 +1,22 @@
 import Navbar from "@/components/navigation/navbar";
 import { Subnav } from "@/components/navigation/subnav";
 import { SignOutButton } from "@/components/sign-out-button";
-import { getCurrentUser } from "@/lib/current-user";
-import { auth } from "@clerk/nextjs";
 import { NextPage } from "next";
 
 const MainLayout: NextPage<{ children: React.ReactNode }> = async ({ children }) => {
-  const currentUser = await getCurrentUser();
-  const { userId } = auth();
-
-  if (userId && currentUser?.userId !== userId)
-    return (
-      <div className="w-full h-screen">
-        <div className="grid place-items-center h-full">
-          <div className="grid grid-flow-row place-items-center gap-2">
-            <p className="w-max">Couldn&apos;t authenticate your connection</p>
-            <p className="w-max">Try refreshing</p>
-            <p>or</p>
-            <SignOutButton text="Sign out and return home" afterSignOutUrl="/" />
-          </div>
-        </div>
-      </div>
-    );
+  // if (userId && currentUser?.userId !== userId)
+  //   return (
+  //     <div className="w-full h-screen">
+  //       <div className="grid place-items-center h-full">
+  //         <div className="grid grid-flow-row place-items-center gap-2">
+  //           <p className="w-max">Couldn&apos;t authenticate your connection</p>
+  //           <p className="w-max">Try refreshing</p>
+  //           <p>or</p>
+  //           <SignOutButton text="Sign out and return home" afterSignOutUrl="/" />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
 
   return (
     <div className="w-full h-screen">
