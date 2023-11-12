729ad2e60c008e2bb29a7c02acc94e536e1641fb
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-12 11:13:36 -0600

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,22 @@
+import { SignOutButton } from "@/components/auth/sign-out-button";
 import Navbar from "@/components/navigation/navbar";
 import { Subnav } from "@/components/navigation/subnav";
+import { getServerCurrentUser } from "@/lib/current-user";
 import { NextPage } from "next";
 
 const MainLayout: NextPage<{ children: React.ReactNode }> = async ({ children }) => {
+  const currentUser = await getServerCurrentUser();
+
+  if (currentUser && !currentUser.active)
+    return (
+      <div className="w-full h-screen grid place-items-center">
+        <div className="w-max grid grid-flow-row gap-2 place-items-center">
+          <p>Your account hasn&apos;t been activated</p>
+          <SignOutButton afterSignOutUrl="/" text="Sign out and return home" />
+        </div>
+      </div>
+    );
+
   return (
     <div className="w-full h-screen">
       <Navbar />
