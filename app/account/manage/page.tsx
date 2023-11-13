338c065baade1ad56a169736df343b4ab9dd8932
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-12 20:59:29 -0600

    Update page.tsx

--- app/account/manage/page.tsx
+++ app/account/manage/page.tsx
@@ -4,9 +4,11 @@ const ManageUserPage: NextPage = () => {
   return (
     <div className="w-max mx-auto">
       <div className="grid grid-flow-col sm:grid-cols-[max-content_60vw] lg:grid-cols-[max-content_50vw] xl:grid-cols-[max-content_40vw]">
-        <div>
-          <div>User settings</div>
-          <div>My Account</div>
+        <div className="grid-flow-row grid gap-1 p-2">
+          <div>
+            <p className="uppercase text-xs font-bold text-zinc-500 px-1">User settings</p>
+          </div>
+          <div className="hover:bg-neutral-300 dark:hover:bg-neutral-800 p-1 transition rounded-md w-full cursor-pointer">My Account</div>
         </div>
         <div className="bg-red-400 w-full">
           <p>manage user page</p>
