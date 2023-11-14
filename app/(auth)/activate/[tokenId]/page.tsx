850cd4f017adeaf96a80f28e86371aea403b03f4
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-13 20:36:58 -0600

    Update page.tsx

--- app/(auth)/activate/[tokenId]/page.tsx
+++ app/(auth)/activate/[tokenId]/page.tsx
@@ -1,4 +1,6 @@
+import { Link } from "@/components/link";
 import { db } from "@/lib/db";
+import { Home } from "lucide-react";
 import { NextPage } from "next";
 
 interface ActivateTokenIdPageProps {
@@ -10,8 +12,6 @@ interface ActivateTokenIdPageProps {
 const ActivateTokenIdPage: NextPage<ActivateTokenIdPageProps> = async ({ params }) => {
   const tokenId = params.tokenId;
 
-  console.log("token", tokenId);
-
   const activateToken = await db.activateToken.findUnique({
     where: {
       token: tokenId
@@ -19,24 +19,42 @@ const ActivateTokenIdPage: NextPage<ActivateTokenIdPageProps> = async ({ params 
     select: {
       user: {
         select: {
+          name: true,
           id: true
         }
       }
     }
   });
 
-  console.log(activateToken);
-
-  if (!activateToken)
+  if (!activateToken || !activateToken.user)
     return (
       <div>
         <p>Couldn't activate this user!</p>
       </div>
     );
 
+  await db.user.update({
+    where: {
+      id: activateToken.user.id
+    },
+    data: {
+      active: true
+    }
+  });
+
   return (
-    <div>
-      <p>{activateToken.user?.id}</p>
+    <div className="grid place-items-center grid-flow-row gap-2">
+      <p>
+        Activated user: <span className="font-semibold">{activateToken.user.name}</span>
+      </p>
+      <Link href="/">
+        <button className="bg-emerald-500 rounded-md px-2 hover:bg-emerald-800 transition h-[32px] text-white">
+          <div className="grid grid-cols-[max-content_auto] place-items-center gap-1">
+            <Home className="w-4 h-4" />
+            <span>Return home</span>
+          </div>
+        </button>
+      </Link>
     </div>
   );
 };
