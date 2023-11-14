9ea062d6f4ffa6b4f6021f12ff9f3f5144330acb
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-14 17:11:03 -0600

    Update page.tsx

--- app/(main)/forums/page.tsx
+++ app/(main)/forums/page.tsx
@@ -28,20 +28,20 @@ const ForumsPage: NextPage = async () => {
       <div className="w-full h-[60px] md:h-[100px] forums-info-gradient-bg sm:rounded-md p-2 overflow-auto">
         <div className="grid grid-cols-3 place-items-center h-full px-2 md:px-10">
           <div className="w-max grid grid-cols-[max-content_auto] place-items-center gap-1 fade-up" style={{ animationDelay: "100ms" }}>
-            <BarChartBig className="w-6 h-6 md:w-7 md:h-7 text-white" />
-            <span className="text-md sm:text-lg md:text-2xl font-semibold text-white">
+            <BarChartBig className="w-6 h-6 text-white" />
+            <span className="text-md font-semibold text-white">
               {threadCount} Thread{threadCount !== 1 && "s"}
             </span>
           </div>
           <div className="w-max grid grid-cols-[max-content_auto] place-items-center gap-1 fade-up" style={{ animationDelay: "300ms" }}>
-            <MessagesSquare className="w-6 h-6 md:w-7 md:h-7 text-white" />
-            <span className="text-md sm:text-lg md:text-2xl font-semibold text-white">
+            <MessagesSquare className="w-6 h-6 text-white" />
+            <span className="text-md font-semibold text-white">
               {replyCount} {replyCount !== 1 ? "Replies" : "Reply"}
             </span>
           </div>
           <div className="w-max grid grid-cols-[max-content_auto] place-items-center gap-1 fade-up" style={{ animationDelay: "500ms" }}>
-            <Users className="w-6 h-6 md:w-7 md:h-7 text-white" />
-            <span className="text-md sm:text-lg md:text-2xl font-semibold text-white">
+            <Users className="w-6 h-6 text-white" />
+            <span className="text-md font-semibold text-white">
               {userCount} User{userCount !== 1 && "s"}
             </span>
           </div>
