8665301fd4b355a09c2009830b6bf3c3193318a9
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-15 20:00:47 -0600

    Update customization.tsx

--- components/users/settings/tabs/customization.tsx
+++ components/users/settings/tabs/customization.tsx
@@ -1,18 +1,48 @@
 "use client";
 
+import { useEdgeStore } from "@/components/providers/edgestore-provider";
 import { useUserSettings } from "@/hooks/use-user-settings-store";
 import { cn } from "@/lib/utils";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 export const CustomizationTab: FC = () => {
   const userSettings = useUserSettings();
-
   const isUserSettingsOpen = userSettings.isOpen && userSettings.type === "customization";
+
+  const [file, setFile] = useState<File>();
+  const { edgestore } = useEdgeStore();
 
   return (
     <div className={cn("", isUserSettingsOpen ? "block" : "hidden")}>
       <p>customization</p>
       <p>coming soon!</p>
+
+      <div>
+        <input
+          type="file"
+          onChange={e => {
+            setFile(e.target.files?.[0]);
+          }}
+        />
+        <button
+          onClick={async () => {
+            if (file) {
+              const res = await edgestore.publicFiles.upload({
+                file,
+                onProgressChange: progress => {
+                  // you can use this to show a progress bar
+                  console.log(progress);
+                }
+              });
+              // you can run some server action or api here
+              // to add the necessary data to your database
+              console.log(res);
+            }
+          }}
+        >
+          Upload
+        </button>
+      </div>
     </div>
   );
 };
