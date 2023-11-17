188742696b34b45ed1493f26d3c44fbd795c8aa6
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-17 17:04:25 -0600

    +

---
+++ components/users/settings/edit-profile-picture.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEdgeStore } from "@/components/providers/edgestore-provider";
+import { cn } from "@/lib/utils";
+import { Pencil } from "lucide-react";
+import { FC, useCallback, useState } from "react";
+import { useDropzone, FileWithPath } from "react-dropzone";
+
+export const EditProfilePicture: FC = () => {
+  const { edgestore } = useEdgeStore();
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const handleProfilePictureUpload = async (file: File) => {
+    console.log(file);
+
+    const res = await edgestore.publicFiles.upload({
+      file
+    });
+
+    console.log(res);
+
+    return { url: res.url };
+  };
+
+  const onDrop = useCallback(
+    async (acceptedFiles: FileWithPath[]) => {
+      // get first file
+      const [firstFile] = acceptedFiles;
+      if (firstFile) {
+        const i = await handleProfilePictureUpload(firstFile);
+
+        setImageUrl(i.url);
+      }
+    },
+    [handleProfilePictureUpload]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "image/png": [".png"], "image/jpg": [".jpg"], "image/jpeg": [".jpeg"] },
+    onDrop,
+    maxFiles: 1
+  });
+
+  return (
+    <div {...getRootProps()} className="border-2 border-dashed border-zinc-500 text-center cursor-pointer w-full h-full overflow-hidden rounded-[50%]">
+      <div className="relative w-full h-full">
+        <div className="absolute grid place-items-center w-full h-full">
+          <Pencil />
+        </div>
+        {imageUrl && <img src={imageUrl} alt="Uploaded" className="w-full h-full" />}
+        <input {...getInputProps()} />
+      </div>
+    </div>
+  );
+};
