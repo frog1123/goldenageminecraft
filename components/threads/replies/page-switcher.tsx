4e92341a91cec937d4981b576977431ff0fbdfe4
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-18 18:02:56 -0600

    Update page-switcher.tsx

--- components/threads/replies/page-switcher.tsx
+++ components/threads/replies/page-switcher.tsx
@@ -17,28 +17,35 @@ export const RepliesPageSwitcher: FC<RepliesPageSwitcherProps> = ({ threadId, to
   if (pathnames.length < 5) page = 1;
 
   const totalPages = Math.ceil(totalReplies / 5);
+  const maxCols = Math.min(totalPages, 5);
+
+  const renderSwitchItems = () => {
+    const switchItems = [];
+    if (totalPages <= 5) {
+      for (let i = 1; i <= totalPages; i++) {
+        switchItems.push(<RepliesPageSwitcherItem key={i} threadId={threadId} pageNum={i} />);
+      }
+    } else if (page <= 3) {
+      for (let i = 1; i <= maxCols - 1; i++) {
+        switchItems.push(<RepliesPageSwitcherItem key={i} threadId={threadId} pageNum={i} />);
+      }
+      switchItems.push(<RepliesPageSwitcherItem key={totalPages} threadId={threadId} pageNum={totalPages} />);
+    } else if (page >= totalPages - 2) {
+      switchItems.push(<RepliesPageSwitcherItem key={1} threadId={threadId} pageNum={1} />);
+      for (let i = totalPages - maxCols + 2; i <= totalPages; i++) {
+        switchItems.push(<RepliesPageSwitcherItem key={i} threadId={threadId} pageNum={i} />);
+      }
+    } else {
+      for (let i = page - 2; i <= page + 2; i++) {
+        switchItems.push(<RepliesPageSwitcherItem key={i} threadId={threadId} pageNum={i} />);
+      }
+    }
+    return switchItems;
+  };
 
   return (
-    <div className="bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2 overflow-auto grid grid-flow-row gap-2 w-max mx-auto">
-      <div className="rounded-md overflow-hidden">
-        {totalPages === 1 && (
-          <div className="grid grid-cols-1">
-            <RepliesPageSwitcherItem threadId={threadId} pageNum={page} />
-          </div>
-        )}
-        {totalPages === 2 && page === 1 && (
-          <div className="grid grid-cols-2">
-            <RepliesPageSwitcherItem threadId={threadId} pageNum={page} />
-            <RepliesPageSwitcherItem threadId={threadId} pageNum={page + 1} />
-          </div>
-        )}
-        {totalPages === 2 && page === 2 && (
-          <div className="grid grid-cols-2">
-            <RepliesPageSwitcherItem threadId={threadId} pageNum={page - 1} />
-            <RepliesPageSwitcherItem threadId={threadId} pageNum={page} />
-          </div>
-        )}
-      </div>
+    <div className="bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2 overflow-auto w-max mx-auto">
+      <div className={`grid grid-flow-row grid-cols-${maxCols} rounded-md overflow-hidden`}>{renderSwitchItems()}</div>
     </div>
   );
 };
