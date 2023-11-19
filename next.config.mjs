ca613764aef47429337b5aaec98e21793f355703
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-19 09:47:15 -0600

    +

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-const path = require("path");
+import path from "path";
+import { fileURLToPath } from "url";
+import million from "million/compiler";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")]
   },
@@ -27,3 +32,10 @@ module.exports = {
   //   ];
   // }
 };
+
+const millionConfig = {
+  auto: { rsc: true },
+  mute: true
+};
+
+export default million.next(nextConfig, millionConfig);
