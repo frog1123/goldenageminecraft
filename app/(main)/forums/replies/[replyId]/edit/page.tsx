2b112911af056082ebcb73ffbcfdd00b2fa1af2a
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-21 19:31:20 -0600

    +

--- app/(main)/forums/replies/[replyId]/edit/page.tsx
+++ app/(main)/forums/replies/[replyId]/edit/page.tsx
@@ -1,4 +1,7 @@
+import { getServerCurrentUser } from "@/lib/current-user";
+import { db } from "@/lib/db";
 import { NextPage } from "next";
+import { redirect } from "next/navigation";
 
 interface ReplyIdEditPageProps {
   params: {
@@ -6,7 +9,23 @@ interface ReplyIdEditPageProps {
   };
 }
 
-const ReplyIdEditPage: NextPage<ReplyIdEditPageProps> = ({ params }) => {
+const ReplyIdEditPage: NextPage<ReplyIdEditPageProps> = async ({ params }) => {
+  const currentUser = await getServerCurrentUser();
+
+  const thread = await db.threadReply.findUnique({
+    where: {
+      id: params.replyId
+    },
+    select: {
+      id: true,
+      authorId: true,
+      content: true
+    }
+  });
+
+  if (!currentUser || thread?.authorId !== currentUser.id) return redirect("/");
+  if (!thread) return redirect("/");
+
   return (
     <div>
       <p>edit {params.replyId}</p>
