64bca188daf84df2e8eea79d859192f8d328e76c
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-22 16:20:58 -0600

    Update page.tsx

--- app/(main)/forums/threads/[threadId]/[pageNum]/page.tsx
+++ app/(main)/forums/threads/[threadId]/[pageNum]/page.tsx
@@ -5,7 +5,6 @@ import { ThreadExpandedSignedType, ThreadExpandedUnsignedType, ThreadVoteStats }
 import { ThreadExpanded } from "@/components/threads/thread-expanded";
 import { ReplyThreadForm } from "@/components/threads/replies/reply-form";
 import { ThreadReplies } from "@/components/threads/replies/thread-replies";
-import { isPositiveWholeNumber } from "@/utils/is-positive-whole";
 import { RepliesPageSwitcher } from "@/components/threads/replies/page-switcher";
 
 interface ThreadIdPageNumPageProps {
@@ -19,117 +18,49 @@ const ThreadIdPageNumPage: NextPage<ThreadIdPageNumPageProps> = async ({ params 
   const currentUser = await getServerCurrentUser();
   const signedIn = !!currentUser;
 
-  let thread: ThreadExpandedSignedType | ThreadExpandedUnsignedType | null;
-
-  if (!isPositiveWholeNumber(params.pageNum)) return <p>invalid page number</p>;
-
-  if (signedIn) {
-    thread = await db.thread.findUnique({
-      where: {
-        id: params.threadId
-      },
-      include: {
-        author: {
-          select: {
-            id: true,
-            name: true,
-            firstName: true,
-            lastName: true,
-            imageUrl: true,
-            rank: true,
-            role: true,
-            plan: true,
-            _count: {
-              select: {
-                threads: true
-              }
-            },
-            threads: {
-              select: {
-                _count: {
-                  select: {
-                    upvotes: true,
-                    downvotes: true
-                  }
-                }
-              }
-            },
-            createdAt: true
-          }
-        },
-        tags: {
-          select: {
-            id: true,
-            name: true
-          }
-        },
-        upvotes: {
-          where: {
-            authorId: currentUser.id
-          }
-        },
-        downvotes: {
-          where: {
-            authorId: currentUser.id
-          }
-        },
-        _count: {
-          select: {
-            upvotes: true,
-            downvotes: true
-          }
+  // no count yet
+  const thread: Omit<ThreadExpandedSignedType, "count"> | Omit<ThreadExpandedUnsignedType, "count"> | null = await db.thread.findUnique({
+    where: {
+      id: params.threadId
+    },
+    select: {
+      title: true,
+      content: true,
+      author: {
+        select: {
+          id: true,
+          name: true,
+          firstName: true,
+          lastName: true,
+          imageUrl: true,
+          rank: true,
+          role: true,
+          plan: true,
+          _count: {
+            select: {
+              threads: true
+            }
+          },
+          createdAt: true
         }
-      }
-    });
-  } else {
-    thread = await db.thread.findUnique({
-      where: {
-        id: params.threadId
       },
-      include: {
-        author: {
-          select: {
-            id: true,
-            name: true,
-            firstName: true,
-            lastName: true,
-            imageUrl: true,
-            rank: true,
-            role: true,
-            plan: true,
-            _count: {
-              select: {
-                threads: true
-              }
-            },
-            threads: {
-              select: {
-                _count: {
-                  select: {
-                    upvotes: true,
-                    downvotes: true
-                  }
-                }
-              }
-            },
-            createdAt: true
-          }
-        },
-        tags: {
-          select: {
-            id: true,
-            name: true
-          }
-        },
-        _count: {
-          select: {
-            upvotes: true,
-            downvotes: true
-          }
+      tags: {
+        select: {
+          id: true,
+          name: true
         }
-      }
-    });
-  }
+      },
+      editedAt: true,
+      createdAt: true
+    }
+  });
+
+  if (!thread)
+    return (
+      <div className="bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2">
+        <p className="text-center">:( Thread does not exist or cannot be found</p>
+      </div>
+    );
 
   const threadReplies = await db.threadReply.count({
     where: {
@@ -142,25 +73,77 @@ const ThreadIdPageNumPage: NextPage<ThreadIdPageNumPageProps> = async ({ params 
     receivedDownvotes: 0
   };
 
-  thread?.author?.threads.forEach(thread => {
-    voteStats.receivedUpvotes += thread._count.upvotes;
-    voteStats.receivedDownvotes += thread._count.downvotes;
+  const authorUpvoteCount = await db.thread.findMany({
+    where: {
+      authorId: thread?.author.id
+    },
+    select: {
+      _count: {
+        select: {
+          votes: {
+            where: {
+              type: "UPVOTE"
+            }
+          }
+        }
+      }
+    }
   });
 
-  const canEdit = thread?.author.id === currentUser?.id;
+  const authorDownvoteCount = await db.thread.findMany({
+    where: {
+      authorId: thread?.author.id
+    },
+    select: {
+      _count: {
+        select: {
+          votes: {
+            where: {
+              type: "UPVOTE"
+            }
+          }
+        }
+      }
+    }
+  });
 
-  if (!thread)
-    return (
-      <div className="bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2">
-        <p className="text-center">:( Thread does not exist or cannot be found</p>
-      </div>
-    );
+  const signedInVote = thread?.author.id
+    ? await db.vote.findUnique({
+        where: {
+          threadId_authorId: {
+            threadId: params.threadId,
+            authorId: thread?.author.id
+          }
+        }
+      })
+    : null;
+
+  console.log(authorUpvoteCount, authorDownvoteCount);
+
+  authorUpvoteCount.forEach(thread => {
+    voteStats.receivedUpvotes += thread._count.votes;
+  });
+
+  authorDownvoteCount.forEach(thread => {
+    voteStats.receivedDownvotes += thread._count.votes;
+  });
+
+  const formattedThread: ThreadExpandedSignedType | ThreadExpandedUnsignedType = {
+    ...thread,
+    count: {
+      upvotes: voteStats.receivedUpvotes,
+      downvotes: voteStats.receivedDownvotes
+    },
+    signedInVote
+  };
+
+  const canEdit = thread?.author.id === currentUser?.id;
 
   return (
     <div className="grid grid-flow-row gap-2">
       <RepliesPageSwitcher totalReplies={threadReplies} threadId={params.threadId} />
-      <ThreadExpanded thread={thread} voteStats={voteStats} canEdit={canEdit} signedIn={signedIn} />
-      <ThreadReplies threadId={params.threadId} tk={5} sk={(parseInt(params.pageNum) - 1) * 5} />
+      <ThreadExpanded thread={formattedThread} voteStats={voteStats} canEdit={canEdit} signedIn={signedIn} />
+      <ThreadReplies threadId={params.threadId} tk={5} sk={0} />
       <ReplyThreadForm threadId={params.threadId} />
       <RepliesPageSwitcher totalReplies={threadReplies} threadId={params.threadId} />
     </div>
