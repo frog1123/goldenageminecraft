b1b7860aca3dcd02b2997d3cfa281148edb76e31
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-22 20:18:46 -0600

    Update route.ts

--- app/api/threads/replies/votes/route.ts
+++ app/api/threads/replies/votes/route.ts
@@ -1,124 +1,124 @@
-import { getServerCurrentUser } from "@/lib/current-user";
-import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
+// import { getServerCurrentUser } from "@/lib/current-user";
+// import { db } from "@/lib/db";
+// import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  try {
-    const res = await req.json();
-    const { replyId, type } = res;
+// export async function POST(req: Request) {
+//   try {
+//     const res = await req.json();
+//     const { replyId, type } = res;
 
-    const currentUser = await getServerCurrentUser();
+//     const currentUser = await getServerCurrentUser();
 
-    if (!currentUser) return new NextResponse("Unauthorized", { status: 401 });
-    if (!currentUser.active) return new NextResponse("Unauthorized", { status: 401 });
-    if (!replyId) return new NextResponse("Bad request", { status: 400 });
+//     if (!currentUser) return new NextResponse("Unauthorized", { status: 401 });
+//     if (!currentUser.active) return new NextResponse("Unauthorized", { status: 401 });
+//     if (!replyId) return new NextResponse("Bad request", { status: 400 });
 
-    const existingThreadUpvote = await db.threadUpvote.findUnique({
-      where: {}
-    });
+//     const existingThreadUpvote = await db.threadUpvote.findUnique({
+//       where: {}
+//     });
 
-    const existingThreadDownvote = await db.threadDownVote.findUnique({
-      where: {
-        threadId_authorId: {
-          threadId,
-          authorId: currentUser.id
-        }
-      }
-    });
+//     const existingThreadDownvote = await db.threadDownVote.findUnique({
+//       where: {
+//         threadId_authorId: {
+//           threadId,
+//           authorId: currentUser.id
+//         }
+//       }
+//     });
 
-    if (type === "u") {
-      if (existingThreadUpvote) return new NextResponse("Thread already upvoted", { status: 400 });
+//     if (type === "u") {
+//       if (existingThreadUpvote) return new NextResponse("Thread already upvoted", { status: 400 });
 
-      if (existingThreadDownvote)
-        await db.threadDownVote.delete({
-          where: {
-            threadId_authorId: {
-              threadId,
-              authorId: currentUser.id
-            }
-          }
-        });
+//       if (existingThreadDownvote)
+//         await db.threadDownVote.delete({
+//           where: {
+//             threadId_authorId: {
+//               threadId,
+//               authorId: currentUser.id
+//             }
+//           }
+//         });
 
-      await db.threadUpvote.create({
-        data: {
-          threadId,
-          authorId: currentUser.id
-        }
-      });
-    } else if (type === "d") {
-      if (existingThreadDownvote) return new NextResponse("Thread already downvoted", { status: 400 });
+//       await db.threadUpvote.create({
+//         data: {
+//           threadId,
+//           authorId: currentUser.id
+//         }
+//       });
+//     } else if (type === "d") {
+//       if (existingThreadDownvote) return new NextResponse("Thread already downvoted", { status: 400 });
 
-      if (existingThreadUpvote)
-        await db.threadUpvote.delete({
-          where: {
-            threadId_authorId: {
-              threadId,
-              authorId: currentUser.id
-            }
-          }
-        });
+//       if (existingThreadUpvote)
+//         await db.threadUpvote.delete({
+//           where: {
+//             threadId_authorId: {
+//               threadId,
+//               authorId: currentUser.id
+//             }
+//           }
+//         });
 
-      await db.threadDownVote.create({
-        data: {
-          threadId,
-          authorId: currentUser.id
-        }
-      });
-    }
+//       await db.threadDownVote.create({
+//         data: {
+//           threadId,
+//           authorId: currentUser.id
+//         }
+//       });
+//     }
 
-    return new NextResponse("Success", { status: 200 });
-  } catch (err) {
-    console.log("[VOTES_POST]", err);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-}
+//     return new NextResponse("Success", { status: 200 });
+//   } catch (err) {
+//     console.log("[VOTES_POST]", err);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
 
-export async function DELETE(req: Request) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const threadId = searchParams.get("t");
-    const type = searchParams.get("ty");
+// export async function DELETE(req: Request) {
+//   try {
+//     const { searchParams } = new URL(req.url);
+//     const threadId = searchParams.get("t");
+//     const type = searchParams.get("ty");
 
-    const currentUser = await getServerCurrentUser();
-    if (!currentUser || !currentUser.active) return new NextResponse("Unauthorized", { status: 401 });
+//     const currentUser = await getServerCurrentUser();
+//     if (!currentUser || !currentUser.active) return new NextResponse("Unauthorized", { status: 401 });
 
-    if (!threadId) return new NextResponse("Bad request", { status: 400 });
+//     if (!threadId) return new NextResponse("Bad request", { status: 400 });
 
-    if (type === "u") {
-      try {
-        await db.threadUpvote.delete({
-          where: {
-            threadId_authorId: {
-              threadId,
-              authorId: currentUser.id
-            }
-          }
-        });
-      } catch (err) {
-        console.log("[VOTES_DELETE]", err);
-        return new NextResponse("Internal Error", { status: 500 });
-      }
-    }
+//     if (type === "u") {
+//       try {
+//         await db.threadUpvote.delete({
+//           where: {
+//             threadId_authorId: {
+//               threadId,
+//               authorId: currentUser.id
+//             }
+//           }
+//         });
+//       } catch (err) {
+//         console.log("[VOTES_DELETE]", err);
+//         return new NextResponse("Internal Error", { status: 500 });
+//       }
+//     }
 
-    if (type === "d") {
-      try {
-        await db.threadDownVote.delete({
-          where: {
-            threadId_authorId: {
-              threadId,
-              authorId: currentUser.id
-            }
-          }
-        });
-      } catch (err) {
-        console.log("[VOTES_DELETE]", err);
-        return new NextResponse("Internal Error", { status: 500 });
-      }
-    }
+//     if (type === "d") {
+//       try {
+//         await db.threadDownVote.delete({
+//           where: {
+//             threadId_authorId: {
+//               threadId,
+//               authorId: currentUser.id
+//             }
+//           }
+//         });
+//       } catch (err) {
+//         console.log("[VOTES_DELETE]", err);
+//         return new NextResponse("Internal Error", { status: 500 });
+//       }
+//     }
 
-    return new NextResponse("Success", { status: 200 });
-  } catch (err) {
-    console.log("[VOTES_DELETE]", err);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-}
+//     return new NextResponse("Success", { status: 200 });
+//   } catch (err) {
+//     console.log("[VOTES_DELETE]", err);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
