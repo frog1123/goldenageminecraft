22c9c11b87b6ce0bc399ebaaf04a0f25ae23ba89
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-23 17:18:51 -0600

    +

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,10 @@
-import { SignUpForm } from "@/components/auth/sign-up-form";
+import { SignUpFormGeneral } from "@/components/auth/sign-up-form-general";
 import { NextPage } from "next";
 
 const SignUpPage: NextPage = () => {
   return (
     <div className="w-full sm:w-[400px] mx-auto">
-      <SignUpForm />
+      <SignUpFormGeneral />
     </div>
   );
 };
---
+++ components/auth/sign-up-form-general.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { logo } from "@/lib/logo";
+import { FC } from "react";
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+import google from "@/public/assets/google.png";
+import { Mail } from "lucide-react";
+import { Link } from "@/components/link";
+
+export const SignUpFormGeneral: FC = () => {
+  return (
+    <div className="bg-neutral-200 dark:bg-neutral-900 sm:rounded-md overflow-hidden">
+      <div className="bg-emerald-500 h-1 hidden sm:block"></div>
+      <div className="p-4">
+        <div className="grid grid-cols-[max-content_auto] gap-1 mx-auto w-max place-items-center">
+          <div className="w-5 h-5 relative">
+            <Image src={logo()} alt="logo" fill />
+          </div>
+          <span className="uppercase text-xl font-bold text-zinc-500 dark:text-white">Register</span>
+        </div>
+        <div className="grid grid-flow-row gap-2 mt-4">
+          <Link href="/sign-up/email">
+            <button className="text-white bg-emerald-500 hover:bg-emerald-800 transition p-2 rounded-md w-full">
+              <div className="grid grid-cols-[max-content_auto] place-items-center gap-1">
+                <Mail className="w-4 h-4" />
+                <span>Sign up with email</span>
+              </div>
+            </button>
+          </Link>
+          <button onClick={() => signIn("google")} className="text-black bg-white hover:bg-gray-300 transition p-2 rounded-md w-full">
+            <div className="grid grid-cols-[max-content_auto] place-items-center gap-1">
+              <div className="w-4 h-4 relative">
+                <Image src={google} fill alt="google" />
+              </div>
+              <span>Sign up with google</span>
+            </div>
+          </button>
+          <p className="uppercase text-xs font-bold text-zinc-500 dark:text-white text-center">
+            Have an account?{" "}
+            <Link href="/sign-in" className="text-blue-500 hover:text-blue-600 transition">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
