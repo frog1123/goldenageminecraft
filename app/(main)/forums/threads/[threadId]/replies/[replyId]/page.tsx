46bd8239fb15072829e89c457d68a478b8ffa030
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-22 19:02:06 -0600

    Update page.tsx

--- app/(main)/forums/threads/[threadId]/replies/[replyId]/page.tsx
+++ app/(main)/forums/threads/[threadId]/replies/[replyId]/page.tsx
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { NextPage } from "next";
 
 interface ReplyIdPageProps {
@@ -7,11 +8,39 @@ interface ReplyIdPageProps {
   };
 }
 
-const ReplyIdPage: NextPage<ReplyIdPageProps> = ({ params }) => {
+const ReplyIdPage: NextPage<ReplyIdPageProps> = async ({ params }) => {
+  const reply = await db.threadReply.findFirst({
+    where: {
+      id: params.replyId
+    },
+    select: {
+      createdAt: true
+    }
+  });
+
+  if (!reply)
+    return (
+      <div className="bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2">
+        <p className="text-center">:( Reply does not exist or cannot be found</p>
+      </div>
+    );
+
+  const replyCreatedAt = reply.createdAt;
+
+  const position = await db.threadReply.count({
+    where: {
+      threadId: params.threadId,
+      createdAt: {
+        lte: replyCreatedAt
+      }
+    }
+  });
+
   return (
     <div>
       <p>thread {params.threadId}</p>
       <p>reply {params.replyId}</p>
+      <p>reply position {position}</p>
     </div>
   );
 };
